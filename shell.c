61bbc6b9d78aa829b7ccc2fb3084e54a2bac2844
emersion <contact@emersion.fr>
2018-06-16 23:43:47 +0100

    Execute brace groups and if clauses

--- shell.c
+++ shell.c
@@ -42,15 +42,52 @@ static int run_simple_command(struct mrsh_state *state,
 	}
 }
 
+static int run_node(struct mrsh_state *state, struct mrsh_node *node);
+
+static int run_command_list_array(struct mrsh_state *state,
+		struct mrsh_array *array) {
+	int exit_status = 0;
+	for (size_t i = 0; i < array->len; ++i) {
+		struct mrsh_command_list *list = array->data[i];
+		exit_status = run_node(state, list->node);
+	}
+	return exit_status;
+}
+
+static int run_command(struct mrsh_state *state, struct mrsh_command *cmd);
+
+static int run_if_clause(struct mrsh_state *state, struct mrsh_if_clause *ic) {
+	int condition_status = run_command_list_array(state, &ic->condition);
+	if (condition_status < 0) {
+		return condition_status;
+	}
+
+	if (condition_status == 0) {
+		return run_command_list_array(state, &ic->body);
+	} else {
+		if (ic->else_part == NULL) {
+			return 0;
+		}
+		return run_command(state, ic->else_part);
+	}
+}
+
 static int run_command(struct mrsh_state *state, struct mrsh_command *cmd) {
 	switch (cmd->type) {
 	case MRSH_SIMPLE_COMMAND:;
 		struct mrsh_simple_command *sc = mrsh_command_get_simple_command(cmd);
 		assert(sc != NULL);
 		return run_simple_command(state, sc);
-	default:
-		assert(false); // TODO
+	case MRSH_BRACE_GROUP:;
+		struct mrsh_brace_group *bg = mrsh_command_get_brace_group(cmd);
+		assert(bg != NULL);
+		return run_command_list_array(state, &bg->body);
+	case MRSH_IF_CLAUSE:;
+		struct mrsh_if_clause *ic = mrsh_command_get_if_clause(cmd);
+		assert(ic != NULL);
+		return run_if_clause(state, ic);
 	}
+	assert(false);
 }
 
 static int run_pipeline(struct mrsh_state *state, struct mrsh_pipeline *pl) {
@@ -65,8 +102,6 @@ static int run_pipeline(struct mrsh_state *state, struct mrsh_pipeline *pl) {
 	}
 	return exit_status;
 }
-
-static int run_node(struct mrsh_state *state, struct mrsh_node *node);
 
 static int run_binop(struct mrsh_state *state, struct mrsh_binop *binop) {
 	int left_status = run_node(state, binop->left);
@@ -112,10 +147,5 @@ int mrsh_run_command_list(struct mrsh_state *state,
 }
 
 int mrsh_run_program(struct mrsh_state *state, struct mrsh_program *prog) {
-	int exit_status = 0;
-	for (size_t i = 0; i < prog->body.len; ++i) {
-		struct mrsh_command_list *list = prog->body.data[i];
-		exit_status = run_node(state, list->node);
-	}
-	return exit_status;
+	return run_command_list_array(state, &prog->body);
 }

46d23dc03e6d27a45c7ec4201f3f9e12eca75524
emersion <contact@emersion.fr>
2018-07-28 18:23:07 +0100

    Fork in a subshell when executing asynchronous lists

--- shell/task_bg.c
+++ shell/task_bg.c
@@ -1,17 +1,83 @@
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "shell.h"
 
 struct task_bg {
 	struct task task;
 	struct task *bg;
+	bool started;
 };
+
+static int fork_detached(void) {
+	pid_t pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "failed to fork(): %s\n", strerror(errno));
+		return -1;
+	} else if (pid == 0) {
+		pid_t child_pid = fork();
+		if (child_pid < 0) {
+			fprintf(stderr, "failed to fork(): %s\n", strerror(errno));
+			return -1;
+		} else if (child_pid == 0) {
+			return 0;
+		} else {
+			exit(EXIT_SUCCESS);
+		}
+	} else {
+		if (waitpid(pid, NULL, 0) == -1) {
+			fprintf(stderr, "failed to waitpid(): %s\n", strerror(errno));
+			return -1;
+		}
+
+		return 1;
+	}
+}
+
+static bool task_bg_start(struct task *task, struct context *ctx) {
+	struct task_bg *tb = (struct task_bg *)task;
+
+	// Start a subshell
+	int ret = fork_detached();
+	if (ret < 0) {
+		return false;
+	} else if (ret == 0) {
+		if (ctx->stdin_fileno >= 0) {
+			dup2(ctx->stdin_fileno, STDIN_FILENO);
+		}
+		if (ctx->stdout_fileno >= 0) {
+			dup2(ctx->stdout_fileno, STDOUT_FILENO);
+		}
+
+		int ret = task_run(tb->bg, ctx);
+		if (ret < 0) {
+			exit(127);
+		}
+
+		exit(ret);
+	} else {
+		if (ctx->stdin_fileno >= 0) {
+			close(ctx->stdin_fileno);
+		}
+		if (ctx->stdout_fileno >= 0) {
+			close(ctx->stdout_fileno);
+		}
+
+		return true;
+	}
+}
 
 static int task_bg_poll(struct task *task, struct context *ctx) {
 	struct task_bg *tb = (struct task_bg *)task;
 
-	int status = task_poll(tb->bg, ctx);
-	if (status == TASK_STATUS_ERROR) {
-		return status;
+	if (!tb->started) {
+		if (!task_bg_start(task, ctx)) {
+			return TASK_STATUS_ERROR;
+		}
+		tb->started = true;
 	}
 
 	return 0;

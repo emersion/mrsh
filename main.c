a56dcee011a345db2b551bec9c05da0b7a677414
Drew DeVault <sir@cmpwn.com>
2018-07-30 21:52:18 -0400

    Add -c, -s, and mrsh [command] to main
    Note that mrsh [command] [args...] is not supported, as mrsh does not
    yet support setting variables.

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
-#define _POSIX_C_SOURCE 2
+#define _POSIX_C_SOURCE 200809L
+#include <assert.h>
+#include <errno.h>
 #include <mrsh/ast.h>
 #include <mrsh/parser.h>
 #include <mrsh/shell.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 static const char usage[] = "usage: mrsh [-n]\n";
@@ -10,11 +13,26 @@ static const char usage[] = "usage: mrsh [-n]\n";
 int main(int argc, char *argv[]) {
 	bool noexec = false;
 
+	struct mrsh_state state = {0};
+	mrsh_state_init(&state);
+
+	FILE *input = stdin;
+
 	int opt;
-	while ((opt = getopt(argc, argv, "n")) != -1) {
+	while ((opt = getopt(argc, argv, "c:ns")) != -1) {
 		switch (opt) {
 		case 'n':
 			noexec = true;
+			break;
+		case 'c':
+			input = fmemopen(optarg, strlen(optarg), "r");
+			if (!input) {
+				fprintf(stderr, "fmemopen failed: %s", strerror(errno));
+				return EXIT_FAILURE;
+			}
+			break;
+		case 's':
+			input = stdin;
 			break;
 		default:
 			fprintf(stderr, usage);
@@ -22,10 +40,17 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	struct mrsh_parser *parser = mrsh_parser_create(stdin);
-	struct mrsh_state state = {0};
-	mrsh_state_init(&state);
+	if (optind < argc) {
+		input = fopen(argv[optind], "r");
+		if (!input) {
+			fprintf(stderr, "could not open %s for reading: %s",
+					argv[optind], strerror(errno));
+			return EXIT_FAILURE;
+		}
+		assert(optind + 1 >= argc && "additional args not yet supported");
+	}
 
+	struct mrsh_parser *parser = mrsh_parser_create(input);
 	while (state.exit == -1) {
 		struct mrsh_program *prog = mrsh_parse_line(parser);
 		if (prog == NULL) {
@@ -40,5 +65,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	mrsh_parser_destroy(parser);
+	fclose(input);
 	return state.exit;
 }

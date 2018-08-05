fcb02874bd9cec9cc7f11ce228e0c7b984016b2b
Drew DeVault <sir@cmpwn.com>
2018-08-05 09:46:02 -0400

    Partially implement $PS1
    The parser needs some work before we can support shell expansion on the
    fly. I think mrsh should include an implementation of wordexp.

--- main.c
+++ main.c
@@ -9,6 +9,17 @@
 #include <unistd.h>
 #include "builtin.h"
 
+static void print_ps1(struct mrsh_state *state) {
+	const char *ps1 = mrsh_hashtable_get(&state->variables, "PS1");
+	if (!ps1) {
+		ps1 = getuid() ? "$ " : "# ";
+	} else {
+		// TODO: Run ps1 through shell expansion
+		// TODO: Replace ! with next history ID
+	}
+	fprintf(stderr, "%s", ps1);
+}
+
 int main(int argc, char *argv[]) {
 	struct mrsh_state state = {0};
 	mrsh_state_init(&state);
@@ -17,6 +28,9 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
+	if (state.interactive) {
+		print_ps1(&state);
+	}
 	struct mrsh_parser *parser = mrsh_parser_create(state.input);
 	while (state.exit == -1) {
 		struct mrsh_program *prog = mrsh_parse_line(parser);
@@ -30,6 +44,9 @@ int main(int argc, char *argv[]) {
 			mrsh_run_program(&state, prog);
 		}
 		mrsh_program_destroy(prog);
+		if (state.interactive) {
+			print_ps1(&state);
+		}
 	}
 
 	mrsh_parser_destroy(parser);

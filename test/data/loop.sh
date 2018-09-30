ea3edb5500a056b1cfa088d1c78f58e754689994
emersion <contact@emersion.fr>
2018-09-30 10:52:40 +0200

    test: add exit in loop test
    Borrowed from https://github.com/emersion/mrsh/issues/35

--- test/data/loop.sh
+++ test/data/loop.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-while [ $n -ne 10 ]; do
-	echo a
+n=asdf
+echo start
+while [ "$n" != "fdsa" ]; do
+	echo "n: $n"
+	n="fdsa"
+	echo "n: $n"
+done
+echo stop
+
+# Should exit immediately
+while true
+do
+	exit
 done

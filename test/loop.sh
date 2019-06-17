d461b314130d16f3fb866680555801ec49772edb
Simon Ser <contact@emersion.fr>
2019-06-17 23:06:14 +0300

    test: add basic break/continue tests

--- test/loop.sh
+++ test/loop.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
+echo "basic while loop"
 n=asdf
 echo start
 while [ "$n" != "fdsa" ]; do
 	echo "n: $n"
 	n="fdsa"
 	echo "n: $n"
+done
+echo stop
+
+echo "continue in while loop should skip the iteration"
+n=asdf
+echo start
+while [ "$n" != fdsa ]; do
+	n=fdsa
+	continue
+	echo "this shouldn't be printed"
+done
+echo stop
+
+echo "break in while loop should stop the loop"
+n=asdf
+echo start
+while true; do
+	if [ "$n" = fdsa ]; then
+		break
+	fi
+	n=fdsa
 done
 echo stop
 

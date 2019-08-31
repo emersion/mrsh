caaa6053b95309576132ce815b0d08f176b3a033
Simon Ser <contact@emersion.fr>
2019-08-31 18:12:19 +0300

    test: enable all pipeline tests

--- test/pipeline.sh
+++ test/pipeline.sh
@@ -9,25 +9,25 @@ echo "a b c d" | sed s/b/B/
 echo "Pipeline with 3 commands"
 echo "a b c d" | sed s/b/B/ | sed s/c/C/
 
-echo >&2 "Pipeline with bang"
+echo "Pipeline with bang"
 ! false
 echo $?
 
 # https://github.com/emersion/mrsh/issues/100
-echo >&2 "Pipeline with subshell"
+echo "Pipeline with subshell"
 (echo "a b"; echo "c d") | sed s/c/C/
 
 # https://github.com/emersion/mrsh/issues/96
-echo >&2 "Pipeline with brace group"
-#{ echo "a b"; echo "c d"; } | sed s/c/C/
+echo "Pipeline with brace group"
+{ echo "a b"; echo "c d"; } | sed s/c/C/
 
 # https://github.com/emersion/mrsh/issues/95
-#echo >&2 "Pipeline with early close"
-#(
-#	i=0
-#	while [ $i -lt 8096 ]
-#	do
-#		echo "Line $i"
-#		i=$((i+1))
-#	done
-#) | head -n 1
+echo "Pipeline with early close"
+(
+	i=0
+	while [ $i -lt 8096 ]
+	do
+		echo "Line $i"
+		i=$((i+1))
+	done
+) | head -n 1

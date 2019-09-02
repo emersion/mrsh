emersion/mrsh

commit 763feb2ee5dde542b3acca8e7f60b37d755a36d2

readline: reset on SIGINT

frontend/readline.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 #if defined(HAVE_READLINE)
 #include <readline/history.h>
 #include <readline/readline.h>
@@ -14,6 +16,17 @@
 #include <histedit.h>
 #endif
 #include "frontend.h"
+
+static void sigint_handler(int n) {
+	/* Signal safety is done here on a best-effort basis. rl_redisplay is not
+	 * signal safe, but under these circumstances it's very likely that the
+	 * interrupted function will not be affected. */
+	char newline = '\n';
+	write(STDOUT_FILENO, &newline, 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 
 static const char *get_history_path(void) {
 	static char history_path[PATH_MAX + 1];
@@ -29,7 +42,11 @@ void interactive_init(struct mrsh_state *state) {
 
 size_t interactive_next(struct mrsh_state *state,
 		char **line, const char *prompt) {
+	struct sigaction sa = { .sa_handler = sigint_handler }, old;
+	sigaction(SIGINT, &sa, &old);
 	char *rline = readline(prompt);
+	sigaction(SIGINT, &old, NULL);
+
 	if (!rline) {
 		return 0;
 	}

commit 658c1ef993757de931ce218626535ce1a5ff8bd0

frontend/readline: fix undefined rl_replace_line with editline
editline doesn't define rl_replace_line. I haven't found a good way to make
SIGINT handling work with editline, let's just disable it for now.

Fixes: 763feb2ee5dd ("readline: reset on SIGINT")

frontend/readline.c
@@ -17,6 +17,7 @@
 #endif
 #include "frontend.h"
 
+#if defined(HAVE_READLINE)
 static void sigint_handler(int n) {
 	/* Signal safety is done here on a best-effort basis. rl_redisplay is not
 	 * signal safe, but under these circumstances it's very likely that the
@@ -27,6 +28,7 @@ static void sigint_handler(int n) {
 	rl_replace_line("", 0);
 	rl_redisplay();
 }
+#endif
 
 static const char *get_history_path(void) {
 	static char history_path[PATH_MAX + 1];
@@ -42,10 +44,15 @@ void interactive_init(struct mrsh_state *state) {
 
 size_t interactive_next(struct mrsh_state *state,
 		char **line, const char *prompt) {
+	/* TODO: make SIGINT handling work with editline */
+#if defined(HAVE_READLINE)
 	struct sigaction sa = { .sa_handler = sigint_handler }, old;
 	sigaction(SIGINT, &sa, &old);
+#endif
 	char *rline = readline(prompt);
+#if defined(HAVE_READLINE)
 	sigaction(SIGINT, &old, NULL);
+#endif
 
 	if (!rline) {
 		return 0;
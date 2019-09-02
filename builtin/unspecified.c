c7c3feb5d57ead83a0146bee215d1eac26f41bc8
Drew DeVault <sir@cmpwn.com>
2019-09-02 15:47:31 +0900

    unspecified builtins: add missing newline

--- builtin/unspecified.c
+++ builtin/unspecified.c
@@ -7,7 +7,7 @@ int builtin_unspecified(struct mrsh_state *state, int argc, char *argv[]) {
 	// Ref: http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_09_01_01
 	if (state->interactive) {
 		fprintf(stderr,
-				"%s: The behavior of this command is undefined.", argv[0]);
+				"%s: The behavior of this command is undefined.\n", argv[0]);
 	} else {
 		fprintf(stderr, "%s: The behavior of this command is undefined. "
 				"This is an error in your script. Aborting.\n", argv[0]);

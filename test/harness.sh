ca14ccf6f07bfe528e3c2436e720063cbcdafafd
Simon Ser <contact@emersion.fr>
2019-09-20 18:04:34 +0300

    meson: add build option for reference shell

--- test/harness.sh
+++ test/harness.sh
@@ -5,7 +5,7 @@ testcase="$1"
 echo "Running with mrsh"
 mrsh_out=$("$MRSH" "$testcase")
 mrsh_ret=$?
-echo "Running with reference shell"
+echo "Running with reference shell ($REF_SH)"
 ref_out=$("$REF_SH" "$testcase")
 ref_ret=$?
 if [ $mrsh_ret -ne $ref_ret ] || [ "$mrsh_out" != "$ref_out" ]
@@ -15,7 +15,7 @@ then
 	echo >&2 "mrsh: $mrsh_ret"
 	echo >&2 "$mrsh_out"
 	echo >&2 ""
-	echo >&2 "ref: $ref_ret"
+	echo >&2 "ref ($REF_SH): $ref_ret"
 	echo >&2 "$ref_out"
 	echo >&2 ""
 	exit 1

d182e3c56c8e94aa4ef6617b03fabb9d91292bc9
Simon Ser <contact@emersion.fr>
2019-11-25 13:05:06 +0100

    test: add multi-line subshell test

--- test/subshell.sh
+++ test/subshell.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Simple subshell"
+(echo hi)
+
 echo "Subshell assignment"
 a=a
 (a=b)
@@ -8,3 +11,9 @@ echo $a
 echo "Subshell status"
 (exit 1) && echo "shouldn't happen"
 true # to clear the last status
+
+echo "Multi-line subshell"
+(
+	echo a
+	echo b
+)

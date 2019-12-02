bfe2f7af47cf5ce1cd141650a1976411e2c1d6aa
Simon Ser <contact@emersion.fr>
2019-12-02 11:18:11 +0100

    test/readonly: make stdout check less strict
    Some shells print the parameter values with quotes [1].
    
    [1]: https://builds.sr.ht/~emersion/job/116952

--- test/readonly.sh
+++ test/readonly.sh
@@ -2,7 +2,7 @@
 
 echo "Print read-only parameters after setting one"
 readonly mrsh_readonly_param=b
-readonly -p | grep mrsh_readonly_param
+readonly -p | grep mrsh_readonly_param | wc -l
 
 #echo "Try setting a read-only parameter"
 #(mrsh_readonly_param=c) 2>/dev/null
